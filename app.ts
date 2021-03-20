5ac61a9e7d573cdda55a0a4b756e18508e074b3b
Zack Stickles <zstickles@newrelic.com>
2021-03-19 17:14:24 -0700

    chore: cleanup

--- app.ts
+++ app.ts
@@ -10,73 +10,7 @@ const app = new App({
   signingSecret: process.env.SIGNING_SECRET,
 });
 
-app.event("app_home_opened", async ({ event, client, context }) => {
-  try {
-    // view.publish is the method that your app uses to push a view to the Home tab
-    await client.views.publish({
-      // user that opened the app's home
-      user_id: event.user,
-      // the view object that appears in the app hom
-      view: {
-        type: "home",
-        callback_id: "home_view",
-
-        // body of the view
-        blocks: [
-          {
-            type: "section",
-            text: { type: "mrkdwn", text: "*Welcome* to _Voltron_ :tada:" },
-          },
-          { type: "divider" },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "A button that does nothing..._yet_:",
-            },
-          },
-          {
-            type: "actions",
-            elements: [
-              {
-                type: "button",
-                text: { type: "plain_text", text: "Click me!" },
-              },
-            ],
-          },
-        ],
-      },
-    });
-  } catch (error) {
-    console.log("[ERROR] Error responding to app_home_opened event");
-    console.error(error);
-  }
-});
-
-app.command("/helloworld", async ({ ack, payload, context }) => {
-  // acknowledge the command request
-  ack();
-
-  try {
-    await app.client.chat.postMessage({
-      token: process.env.ACCESS_TOKEN,
-      channel: payload.channel_id,
-      blocks: [
-        {
-          type: "section",
-          text: { type: "mrkdwn", text: "Hellooooo _world_ :robot_face:" },
-        },
-      ],
-      text: "Message from Test App",
-    });
-  } catch (error) {
-    console.log("[ERROR] Error responding to /helloworld");
-    console.dir(error, { depth: 3 });
-    console.error(error);
-  }
-});
-
-app.command("/voltron", async ({ ack, payload, context }) => {
+app.command("/voltron", async ({ ack, payload }) => {
   ack();
 
   try {
@@ -84,9 +18,24 @@ app.command("/voltron", async ({ ack, payload, context }) => {
     const command = text.split(" ")[0];
 
     if (command !== "cat") {
-      console.log("[Voltron] I only respond with CATS");
+      await app.client.chat.postMessage({
+        token: process.env.ACCESS_TOKEN,
+        channel: payload.channel_id,
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "I only provide cat pictures at the moment.",
+            },
+          },
+        ],
+        text: "Cat picture",
+      });
+
       return;
     }
+
     const resp = await fetch("https://api.thecatapi.com/v1/images/search");
     const data = await resp.json();
     const image_url = data[0].url;
